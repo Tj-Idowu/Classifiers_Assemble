a986ebddfb113f296eadd7f03d78609571fff374
Tj-Idowu <32261407+Tj-Idowu@users.noreply.github.com>
2024-08-06 12:27:16 +0100

    Create CLARK.sh
    Run CLARK with bash using its input .txt file format

---
+++ CLARK.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name=CLARK_zero
+#SBATCH -t 150:00:00
+#SBATCH --nodelist=sonicmem3
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=olateju.idowu@ucdconnect.ie
+
+## Clean up
+./clean.sh
+
+## Update Taxonomy
+./updateTaxonomy.sh
+
+
+## Create and index the database
+./set_targets.sh CLARK_DB bacteria --species
+
+## Classify the samples
+./classify_metagenome.sh -P ../zero_F.txt ../zero_R.txt -R ../zero_results.txt
