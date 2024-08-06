fc2b55e38e55146e87c0030e83251291fc98ac54
Tj-Idowu <32261407+Tj-Idowu@users.noreply.github.com>
2024-08-06 12:09:40 +0100

    Create centrifuge.sh
    Running centrifuge with a sample tsv file using a bash script

---
+++ centrifuge.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH --job-name=centrifuge
+#SBATCH -t 200:00:00
+#SBATCH --ntasks-per-node 5
+#SBATCH -N 4    ## Only set this if you need more than one node for your job, otherwise this line is not required at all
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=olateju.idowu@ucdconnect.ie
+
+## Centrifuge commands
+./centrifuge/centrifuge -x centrifuge_DB/p_compressed --sample-sheet centrifuge_sample-sheet.tsv
