0ba41063e8b9db117ebf741df6ddf43b5bfb268b
Tj-Idowu <32261407+Tj-Idowu@users.noreply.github.com>
2026-09-24 11:07:15 +0100

    Update centrifuge.sh

--- centrifuge.sh
+++ centrifuge.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node 5
 #SBATCH -N 4    ## Only set this if you need more than one node for your job, otherwise this line is not required at all
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=olateju.idowu@ucdconnect.ie
+#SBATCH --mail-user=me@email.ie
 
 ## Centrifuge commands
 ./centrifuge/centrifuge -x centrifuge_DB/p_compressed --sample-sheet centrifuge_sample-sheet.tsv
