bd369213a311af38c620c02838242a23c609396d
Tj-Idowu <32261407+Tj-Idowu@users.noreply.github.com>
2026-09-24 11:09:17 +0100

    Update kma.sh

--- kma.sh
+++ kma.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -l
 #SBATCH --job-name=kma_Yersinia
 #SBATCH -t 120:00:00
-#SBATCH -n 4 # This is the number of cores that you can specify as --p-threads, and they should be the same
+#SBATCH -n 4
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=olateju.idowu@ucdconnect.ie
+#SBATCH --mail-user=me@email.ie
 
 # set input and output paths
-input_dir=/scratch/12355656/Benchmarking/Generator/Metagenomes/Yersinia
-output_dir=/scratch/12355656/Benchmarking/Simulated_Metagenomes/kma_results/Yersinia
-kma=/scratch/12355656/Metagenomes/GalwayWW/kma/kma
+input_dir=Benchmarking/Generator/Metagenomes/Yersinia
+output_dir=Benchmarking/Simulated_Metagenomes/kma_results/Yersinia
+kma=Benchmarking/Metagenomes/GalwayWW/kma/kma
 
 # set file extensions
 r1_extension=_R1.fastq
 r2_extension=_R2.fastq
 
 # set database path
-db_path=/scratch/12355656/compress_ncbi_nt/ncbi_nt
+db_path=Benchmarking/compress_ncbi_nt/ncbi_nt
 
 # loop over input files
 for r1_path in "${input_dir}"/*"${r1_extension}"; do
